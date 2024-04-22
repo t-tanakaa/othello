7f0238bf7f4b532e73337f8e78cc2a85d45e49db
t-tanakaa <s1f102401753@iniad.org>
2024-04-23 08:50:43 +0900

    八方向を考える

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,75 @@
-import { use, useState } from 'react';
+import { useState } from 'react';
 import styles from './index.module.css';
 
+const directions = [
+  [0, 1],
+  [0, -1],
+  [1, 1],
+  [1, -1],
+  [1, 0],
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+];
+
+// const flipStones = (board: number[][], turnColor: number, x: number, y: number) => {};
+
 const Home = () => {
-  const [turncolor,setturncolor]=useState(1)
+  const [turncolor, setturncolor] = useState(1);
   const [board, setboard] = useState([
-    //空白0 白1 黒2 row=行
+    //空白0 白1 黒2 row=行 for 調べる
+    // [0, 0, 0, 0, 0, 0, 0, 0],
+    // [0, 0, 0, 0, 0, 0, 0, 0],
+    // [0, 0, 0, 0, 0, 0, 0, 0],
+    // [0, 0, 0, 1, 2, 0, 0, 0],
+    // [0, 0, 0, 2, 1, 0, 0, 0],
+    // [0, 0, 0, 0, 0, 0, 0, 0],
+    // [0, 0, 0, 0, 0, 0, 0, 0],
+    // [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 2, 0, 0, 0],
-    [0, 0, 0, 2, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 2, 1],
+    [0, 0, 0, 0, 0, 1, 2, 1],
+    [0, 0, 0, 0, 2, 1, 2, 1],
+    [0, 0, 0, 1, 2, 1, 2, 1],
+    [0, 0, 2, 1, 2, 1, 2, 1],
+    [0, 2, 1, 2, 1, 2, 1, 2],
   ]);
+
   const clickHandler = (x: number, y: number) => {
     console.log(x, y);
+
+    let n = 0;
+    let m = 0;
+
+    //console.log(board[y][x])
+    for (const direction of directions) {
+      const yoko = direction[0];
+      const tate = direction[1];
+    }
+    if (board[y][x] !== 0) return;
+    //console.log(100);
     const newboard = structuredClone(board);
-    newboard[y][x] = turncolor
-    if(turncolor===1){
-      setturncolor(2);
-    }else{
-      setturncolor(1);
+
+    while (n < 7) {
+      //console.log(200);
+      n++;
+      if (board[y + n] !== undefined && board[y + n][x] === 2 / turncolor) {
+        //console.log(300);
+        if (board[y + 1 + n] !== undefined && board[y + 1 + n][x] === turncolor) {
+          //console.log(400);
+          newboard[y][x] = turncolor;
+          while (n >= m) {
+            //console.log(500);
+            newboard[y + m][x] = turncolor;
+            m++;
+          }
+          setturncolor(2 / turncolor);
+          break;
+        }
+      } else {
+        break;
+      }
     }
     setboard(newboard);
   };
@@ -45,5 +93,4 @@ const Home = () => {
     </div>
   );
 };
-
 export default Home;
